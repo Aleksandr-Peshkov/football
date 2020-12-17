05a5a42aeb6fb846da58c4388fd77c1d737c5a3c
dvur0g <hoof7@mail.ru>
2020-12-17 21:02:28 +0300

    tr

--- src/main/java/ru/alex/football/FootballApplication.java
+++ src/main/java/ru/alex/football/FootballApplication.java
@@ -3,9 +3,16 @@ package ru.alex.football;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.alex.football.repository.TrainerRepository;
 
 @SpringBootApplication
-public class FootballApplication implements CommandLineRunner{
+public class FootballApplication implements CommandLineRunner {
+
+    FootballApplication(TrainerRepository trainerRepository) {
+        this.trainerRepository = trainerRepository;
+    }
+
+    TrainerRepository trainerRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(FootballApplication.class, args);
